d30e37ec8c29e02a5ec85142056ea2246ad54e3d
Luiz Rosalba <luiz.rosalba@deepseedsolutions.com>
2021-06-02 16:21:54 -0300

    limpando arquivos não usados

--- administrador.cpp
+++
@@ -1,8 +0,0 @@
-#include "administrador.h"
-
-Administrador::Administrador(int id, const QString &nombre,
-                             const QString &contrasenya)
-    : Usuario(id, nombre, contrasenya)
-{
-}
-
--- usuario.cpp
+++
@@ -1,9 +0,0 @@
-#include "usuario.h"
-
-Usuario::Usuario(int id, const QString &nombre, const QString &contrasenya)
-    : mId(id)
-    , mNombre(nombre)
-    , mContrasenya(contrasenya)
-{
-}
-
